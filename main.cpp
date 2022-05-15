e392aeb0f35bde64fca78a098d67b3d4799e94fb
mesor21 <menzdd21@gmail.com>
2022-05-15 23:18:28 +0300

    good

--- main.cpp
+++ main.cpp
@@ -28,28 +28,31 @@ public:
 	void set_second(int second){
 		this->second=second;
 	}
-	int get_hours()const{
-		return hours;
+	int get_hours(){
+		return hours;	
 	}
-	int get_minute()const{
+	int get_minute(){
 		return minute;
 	}
-	int get_second()const{
+	int get_second(){
 		return second;
 	}
 };
 
-Time TimeCall;
-void timeOfCall(Time &start_call, Time &end_call){
-	int call_second = end_call.time_to_second()-start_call.time_to_second();
-	TimeCall.set_hours(call_second/3600);
-	call_second = call_second%3600;
-	TimeCall.set_minute(call_second/60);
-	TimeCall.set_second(call_second%60);
+class TimeCall:public Time{
+public:
+	TimeCall(Time &start_call, Time &end_call){
+		int call_second = end_call.time_to_second()-start_call.time_to_second();
+		if(call_second<0){
+			call_second=call_second+(60*60*24);
+		}
+		set_hours(call_second/3600);
+		call_second = call_second%3600;
+		set_minute(call_second/60);
+		set_second(call_second%60);
+	}
 };
-void print(){
-	cout<<TimeCall.get_hours()<<":"<<TimeCall.get_minute()<<":"<<TimeCall.get_second()<<endl;
-};
+
 int main(){
 	Time start_call;
 	Time end_call;
@@ -57,20 +60,20 @@ int main(){
 	int time_end_call[2];
 	cout<<"example"<<endl;
 	cout<<"12:12:12"<<endl;
-	string call_start;
+	char call_start[8];
 	cout<<"Начало звонка"<<endl;
 	cin>>call_start;
 	time_start_call[0]=((call_start[0]-'0')*10)+call_start[1]-'0';
 	time_start_call[1]=((call_start[3]-'0')*10)+call_start[4]-'0';
 	time_start_call[2]=((call_start[6]-'0')*10)+call_start[7]-'0';
+	start_call.set_time(time_start_call[0],time_start_call[1],time_start_call[2]);
 	cout<<"Конец звонка"<<endl;
-	string call_end;
+	char call_end[8];
 	cin>>call_end;
 	time_end_call[0]=((call_end[0]-'0')*10)+(call_end[1]-'0');
 	time_end_call[1]=((call_end[3]-'0')*10)+(call_end[4]-'0');
 	time_end_call[2]=((call_end[6]-'0')*10)+(call_end[7]-'0');
-	start_call.set_time(time_start_call[0],time_start_call[1],time_start_call[2]);
 	end_call.set_time(time_end_call[0],time_end_call[1],time_end_call[2]);
-	timeOfCall(start_call, end_call);
-	print();
+	TimeCall x(start_call, end_call);
+	cout<<x.get_hours()<<":"<<x.get_minute()<<":"<<x.get_second()<<endl;
 }
